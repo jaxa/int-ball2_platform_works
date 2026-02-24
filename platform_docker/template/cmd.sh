7b15a86ca688747bf5030b3b7b197d416f7485fe
tainirigel <taisei218@gmail.com>
2026-02-24 17:58:34 +0900

    minor modification for ROS Noetic

--- platform_docker/template/cmd.sh
+++ platform_docker/template/cmd.sh
@@ -2,4 +2,4 @@
 
 source ${IB2_WORKSPACE}/devel/setup.bash
 export ROS_IP=`hostname -I | cut -d ' ' -f1`
-/opt/ros/melodic/bin/roslaunch ${IB2_PACKAGE} ${IB2_LAUNCH_FILE}
+/opt/ros/noetic/bin/roslaunch ${IB2_PACKAGE} ${IB2_LAUNCH_FILE}
